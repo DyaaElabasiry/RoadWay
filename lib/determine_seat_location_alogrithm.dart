d3d5046403016d3756f7f307085e3cfe6f0cd3f7
unknown <dyaamohamed86@gmail.com>
2024-06-19 13:11:43 +0300

    completed algorithm

--- lib/determine_seat_location_alogrithm.dart
+++ lib/determine_seat_location_alogrithm.dart
@@ -3,9 +3,11 @@ import 'package:latlong2/latlong.dart';
 import 'dart:math' as math;
 
 void getSeatLocation(DateTime date , LatLng location,LatLng currentLocation,LatLng destinationLocation){
-  var sunPosition = SunCalc.getSunPosition(date,location.latitude, location.longitude);
+  var sunPosition = SunCalc.getSunPosition(date,location.latitude, location.longitude);  // altitude and azimuth of the sun
   double  azimuth = sunPosition['azimuth']! * 180 / math.pi;
   double altitude = sunPosition['altitude']! * 180 / math.pi;
+  String sun_position = '';  // right or left of the rider
+  String sun_intensity = '';  // high or low
   // converting azimuth to 0-360
   if(azimuth>=0){
     azimuth = 270 - azimuth;
@@ -25,5 +27,26 @@ void getSeatLocation(DateTime date , LatLng location,LatLng currentLocation,LatL
     angle = 360 + angle;
   }
   double angleDifference = angle - azimuth;
+  if(angleDifference>0){
+    if(angleDifference<180) {
+      sun_position = 'Right';
+    }else{
+      sun_position = 'Left';
+      angleDifference = 360 - angleDifference;
+    }
+  }else{
+    if(angleDifference>-180) {
+      sun_position = 'Left';
+    }else{
+      sun_position = 'Right';
+      angleDifference = 360 + angleDifference;
+    }
+  }
 
+  if(angleDifference<20 && angleDifference>160) {
+    sun_intensity = 'Low';
+  }else{
+    sun_intensity = 'High';
+  }
+  print('Sun Position: $sun_position and Sun Intensity: $sun_intensity');
 }
